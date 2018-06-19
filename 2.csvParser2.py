66695a4c9c1afeb79c802835c149dada075fd5ff
Muskan Srivastava <34334196+Muskan-S@users.noreply.github.com>
2018-06-19 15:46:43 +0530

    Incremental update of parsed file

--- 2.csvParser2.py
+++ 2.csvParser2.py
@@ -4,29 +4,27 @@ import csv
 import os
 
 usersummary = dict()	
-with open('parse1.csv','r') as csv_file:
+with open('parse1uniq.csv','r') as csv_file:
 	csv_reader = csv.DictReader(csv_file,delimiter=",")
 	for row in csv_reader:
-		if  row['event']=="view_collection":
-	
-			if row['user']=="anonymous":
-				user = row['ip_address']
-			else:
-				user = row['user']
+		if row['user']=="anonymous":
+			user = row['ip_address']
+		else:
+			user = row['user']
 
-			if user in usersummary:
-			#dictionary => key is string and value is set. Set is taken to avoid duplicates else take list.
-				s=usersummary[user]
-				s.add(row['event_id'])
-			else :
-				a=set()
-				a.add(row['event_id'])
-				usersummary[user]=a
-				#usersummary[user]=set(row['event_id']) --> wrong: breaks up row['event_id'] into characters
+		if user in usersummary:
+		#dictionary => key is string and value is set. Set is taken to avoid duplicates else take list.
+			s=usersummary[user]
+			s.add(row['event_id'])
+		else :
+			a=set()
+			a.add(row['event_id'])
+			usersummary[user]=a
+			#usersummary[user]=set(row['event_id']) --> wrong: breaks up row['event_id'] into characters
 
-print "Dictionary"
-for key,value in usersummary.items():
-	print "\n",key,value
+#print "Dictionary"
+#for key,value in usersummary.items():
+#	print "\n",key,value
 	
 
 with open('parse2.csv', 'wb') as csv_file:
@@ -37,4 +35,3 @@ with open('parse2.csv', 'wb') as csv_file:
 		csv_file.write("\n")			
 
 print("CSV file has been parsed sucessfully !!!!***");
-
